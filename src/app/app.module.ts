61c5102e6a54a6969dadf8e8a487ca3ecba03579
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-11-24 22:27:09 +0100

    feat: Project list component, service, model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectServiceModule} from "./services/project.service-module";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 
 @NgModule({
   declarations: [AppComponent],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        EmployeeListComponentModule,
-        EmployeeFacesComponentModule,
-        EmployeeServiceModule
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      EmployeeListComponentModule,
+      EmployeeFacesComponentModule,
+      ProjectListComponentModule,
+      EmployeeServiceModule,
+      ProjectServiceModule
     ],
   providers: [],
   bootstrap: [AppComponent],
