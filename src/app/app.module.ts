812c0b52d5460695af435d62637e7fa5b1b38ac0
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-11-25 19:31:21 +0100

    remove models, components, services, add tailwind css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
-import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectServiceModule} from "./services/project.service-module";
-import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeFacesComponentModule,
-    ProjectListComponentModule,
-    EmployeeServiceModule,
-    ProjectServiceModule,
-    EmployeeFormComponentModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
