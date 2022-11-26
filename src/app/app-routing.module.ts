f39874175ffa33e862f09d0b9915a50f4b826e03
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-11-26 20:26:52 +0100

    feat: employee list and create form in different routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeCreateFormComponent } from './ui/employee-create-form/employee-create-form.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { EmployeeCreateFormComponentModule } from './ui/employee-create-form/employee-create-form.component-module';
+import {HomeComponent} from "./ui/home/home.component";
+import {HomeComponentModule} from "./ui/home/home.component-module";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'create-employee', component: EmployeeCreateFormComponent }, { path: '', component: HomeComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeCreateFormComponentModule, HomeComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
